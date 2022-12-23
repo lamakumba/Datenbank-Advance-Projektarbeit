017f00b7eca10ef36d9d1c580c6837d22d6e63b5
Leandro Pajer <leandropajer@bluewin.ch>
2022-12-23 11:37:56 +0100

    ArtikelVm Prototype

--- Semesterprojekt Datenbank/Semesterprojekt Datenbank/obj/Debug/net6.0-windows/App.g.i.cs
+++ Semesterprojekt Datenbank/Semesterprojekt Datenbank/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace Semesterprojekt_Datenbank {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Semesterprojekt_Datenbank {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
         public static void Main() {
             Semesterprojekt_Datenbank.App app = new Semesterprojekt_Datenbank.App();
             app.InitializeComponent();
